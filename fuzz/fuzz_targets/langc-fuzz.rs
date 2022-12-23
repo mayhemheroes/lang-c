429f56dd2e809143f8f0cf2ce30ebae01ae7b6e8
ysthakur <2004yst@gmail.com>
2022-12-23 15:19:46 -0500

    Choose between gcc or clang

--- fuzz/fuzz_targets/langc-fuzz.rs
+++ fuzz/fuzz_targets/langc-fuzz.rs
@@ -1,15 +1,21 @@
 #![no_main]
-use libfuzzer_sys::fuzz_target;
-use lang_c::driver::{Config, parse_preprocessed}; 
-use std::str;
 
-fuzz_target!(|data: &[u8]| {
-    // fuzzed code goes here
-    match str::from_utf8(data) {
-        Ok(in_string)=>{
-            let config = Config::default();
-            parse_preprocessed(&config, in_string.to_string());
-        },
-        Err(..)=>()
-    }
+use arbitrary::Arbitrary;
+use libfuzzer_sys::fuzz_target;
+
+use lang_c::driver::{parse_preprocessed, Config};
+
+#[derive(Arbitrary, Debug)]
+struct Data {
+    use_gcc: bool,
+    source: String,
+}
+
+fuzz_target!(|data: Data| {
+    let config = if data.use_gcc {
+        Config::with_gcc()
+    } else {
+        Config::with_clang()
+    };
+    let _ = parse_preprocessed(&config, data.source);
 });
